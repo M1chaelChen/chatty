ae5bb3ed4045d753f45e8e1d2fdca44387f3ea0a
Michael Chen <cyjdyj@gmail.com>
2018-01-17 14:38:10 -0800

    init server setup

---
+++ server/data/schema.js
@@ -0,0 +1,10 @@
+const Schema = [
+  `type Query {
+    testString: String
+  }
+  schema {
+    query: Query
+  }`,
+];
+
+module.exports = Schema;
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { graphqlExpress, graphiqlExpress} = require('graphql-server-express');
+const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools');
+const bodyParser = require('body-parser');
+const { createServer } = require('http');
+const Schema = require('./data/schema');
+const Mocks = require('./data/mocks');
+
+const GRAPHQL_PORT = 8080;
+const app = express();
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
+// `context` must be an object and can't be undefined when using connectors
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: executableSchema,
+  context: {}, // at least(!) an empty object
+}));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));
